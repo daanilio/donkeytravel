ed36583cec6523b76bab8cc0ea1922e4d91ef210
Lisaa-vd <vandongenlisa@hotmail.com>
2023-09-25 16:36:34 +0200

    styling update

--- Views/Reserveer/createReservering.php
+++ Views/Reserveer/createReservering.php
@@ -21,35 +21,35 @@ $tochten = new Tochten()
 ?>
 
 <main class="py-18 px-64 flex justify-center">
-    <form class="w-2/5 bg-gray-300 rounded-lg p-12 m-12  " action="createReserveringController.php" method="post">
+    <form class="w-1/2 bg-green-800 rounded-lg p-12 m-12 text-black" action="createReserveringController.php" method="post">
         <div class="mb-5">
-            <label for="voornaam">Voornaam</label>
+            <label class="text-white" for="voornaam">Voornaam</label>
             <input type="text" name="voornaam" id="voornaam"
                    class="p-1 hover:bg-gray-200 border border-gray-700 rounded-md min-w-full">
         </div>
         <div class="mb-5">
-            <label for="achternaam">Achteraam</label>
+            <label class="text-white" for="achternaam">Achteraam</label>
             <input type="text" name="achternaam" id="achternaam"
                    class="p-1 hover:bg-gray-200 border border-gray-700 rounded-md min-w-full">
         </div>
         <div class="mb-5">
-            <label for="email">Email</label>
+            <label class="text-white" for="email">Email</label>
             <input type="text" name="email" id="email"
                    class="p-1 hover:bg-gray-200 border border-gray-700 rounded-md min-w-full">
         </div>
         <div class="mb-5">
-            <label for="personen">Aantal personen</label>
+            <label class="text-white" for="personen">Aantal personen</label>
             <input type="number" name="personen" id="personen"
                    class="p-1 hover:bg-gray-200 border border-gray-700 rounded-md min-w-full">
         </div>
         <div class="mb-5">
-            <label for="tochtSelect">Selecteer een tocht</label><br>
+            <label class="text-white" or="tochtSelect">Selecteer een tocht</label><br>
             <select name="tochten" id="tochtSelect" class="p-1 hover:bg-gray-200 border border-gray-700 rounded-md min-w-full">
             <?php $tochten->getAll() ?>
             </select>
         </div>
         <div class="mb-5">
-            <label for="datum">Startdatum rit</label>
+            <label class="text-white" for="datum">Startdatum rit</label>
             <input type="date" name="datum" id="datum"
                    class="p-1 hover:bg-gray-200 border border-gray-700 rounded-md min-w-full">
         </div>
