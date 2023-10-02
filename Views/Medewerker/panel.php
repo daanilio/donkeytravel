6d0819a04e16ce43f334c092733528c026fb69ba
Lisaa-vd <vandongenlisa@hotmail.com>
2023-10-02 13:37:27 +0200

    Panel reservering is ingesteld

--- Views/Medewerker/panel.php
+++ Views/Medewerker/panel.php
@@ -13,29 +13,48 @@
 <?php include '../../Components/header.php'; ?>
 
 <main class="py-24 px-64">
-    <div class="grid gap-x-20 gap-y-10 grid-cols-2 grid-rows-2">
-        <div class="border-4 border-gray-800 p-3 text-center rounded-xl"><h2 class="text-3xl font-bold"><span
-                        class="border-b-4 border-green-800 pb-1">Medewerker panel</span>
-            </h2>
-            <p class="text-medium"><br>Navigeer hier naar uw gewenste locatie. <br>
-                De pagina verwijst naar een CRUD.</p>
+    <div class="grid gap-x-20 gap-y-10 grid-cols-2 grid-rows-2 ">
+        <div class="bg-gray-200 border-4 border-gray-800 p-3 text-center rounded-xl"><h2 class="text-3xl font-bold"><span
+                        class="border-b-4 border-green-800 pb-1">Medewerker panel</span></h2>
+            <p class="mt-5">Beheer hier de gebruikers, reserveringen en ezels.</p>
         </div>
-        <div class="border-4 border-gray-800 rounded-xl p-3 text-center bg-gray-200">
-            <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-green-600"><span
+        <div class="border-4 border-gray-800 rounded-xl p-3 text-center ">
+            <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-gray-700"><span
                             class="border-b-4 border-green-800"><a href="#">Gebruikers</a></span></h2>
-                <p class="text-medium"><br>Manage de gebruikers/accounts.
+                <div class="mb-4"><br>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="">Create</a></span>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="">Read</a></span>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="">Update/Delete</a></span>
+                </div>
             </div>
         </div>
-        <div class="border-4 border-gray-800 rounded-xl p-3 text-center bg-gray-200">
-            <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-green-600"><span
+        <div class="border-4 border-gray-800 rounded-xl p-3 text-center ">
+            <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-gray-700"><span
                             class="border-b-4 border-green-800"><a href="#">Reserveringen</a></span></h2>
-                <p class="text-medium"><br>Manage de reserveringen.
+                <div class="mb-4"><br>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="../Reserveer/createReservering.php">Create</a></span>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="../Reserveer/readReservering.php">Read</a></span>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="../Reserveer/readReservering.php">Update/Delete</a></span>
+                </div>
             </div>
         </div>
-        <div class="border-4 border-gray-800 rounded-xl p-3 text-center bg-gray-200">
-            <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-green-600"><span
+        <div class="border-4 border-gray-800 rounded-xl p-3 text-center ">
+            <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-gray-700"><span
                             class="border-b-4 border-green-800"><a href="#">Ezels</a></span></h2>
-                <p class="text-medium"><br>Manage onze ezels.
+                <div class="mb-4"><br>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="">Create</a></span>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="">Read</a></span>
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                                href="">Update/Delete</a></span>
+                </div>
             </div>
         </div>
     </div>
