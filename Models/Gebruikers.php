27aa7ca9d2d74d7e25fb01ef47aa4ad5b5485bd6
FlyingWumpus <94541665+FlyingWumpus@users.noreply.github.com>
2023-10-10 12:13:15 +0200

    Create constructor

--- Models/Gebruikers.php
+++ Models/Gebruikers.php
@@ -1,8 +1,24 @@
 <?php
+// Danilio Veldhoen
 
 namespace Models;
 
 class Gebruikers
 {
+    public $id;
+    public $naam;
+    public $email;
+    public $wachtwoord;
 
+    /**
+     * @param $naam
+     * @param $email
+     * @param $wachtwoord
+     */
+    public function __construct($naam = NULL, $email = NULL, $wachtwoord = NULL)
+    {
+        $this->naam = $naam;
+        $this->email = $email;
+        $this->wachtwoord = $wachtwoord;
+    }
 }
