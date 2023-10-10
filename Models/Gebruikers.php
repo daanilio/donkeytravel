fd9daafa07cef13d6e166280a974b62c51d7d6fc
FlyingWumpus <94541665+FlyingWumpus@users.noreply.github.com>
2023-10-10 12:14:04 +0200

    Add create function

--- Models/Gebruikers.php
+++ Models/Gebruikers.php
@@ -21,4 +21,65 @@ class Gebruikers
         $this->email = $email;
         $this->wachtwoord = $wachtwoord;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNaam()
+    {
+        return $this->naam;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWachtwoord()
+    {
+        return $this->wachtwoord;
+    }
+
+
+    public function create()
+    {
+        // Dit zorgt ervoor dat het in de database komt te staan
+        require "../../Database/database.php";
+
+        $id = NULL;
+        $naam = $this->getNaam();
+        $email = $this->getEmail();
+        $wachtwoord = $this->getWachtwoord();
+
+        $sql = $conn->prepare("insert into gebruikers values(:id, :naam, :email, :wachtwoord)");
+
+        $sql->bindParam(":id", $id);
+        $sql->bindParam(":naam", $naam);
+        $sql->bindParam(":email", $email);
+        $sql->bindParam(":wachtwoord", $wachtwoord);
+
+        $sql->execute();
+        ?>
+
+        <html lang="en">
+        <br>
+        <p class="text-center text-2xl">De gebruiker is toegevoegd.</p>
+        </html>
+
+        <?php
+    }
 }
