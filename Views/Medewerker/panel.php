4f2bdbff9b1e962f8861ecb0a87ab45a1b79f1ad
Lisaa-vd <vandongenlisa@hotmail.com>
2023-10-11 13:16:01 +0200

    Knoppies aangepast

--- Views/Medewerker/panel.php
+++ Views/Medewerker/panel.php
@@ -18,41 +18,41 @@
                         class="border-b-4 border-green-800 pb-1">Medewerker panel</span></h2>
             <p class="mt-5">Beheer hier de gebruikers, reserveringen en ezels.</p>
         </div>
-        <div class="border-4 border-gray-800 rounded-xl p-3 text-center ">
+        <div class="bg-gray-200 border-4 border-gray-800 rounded-xl p-3 text-center ">
             <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-gray-700"><span
                             class="border-b-4 border-green-800"><a href="#">Gebruikers</a></span></h2>
                 <div class="mb-4"><br>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="">Create</a></span>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="">Read</a></span>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="">Update/Delete</a></span>
                 </div>
             </div>
         </div>
-        <div class="border-4 border-gray-800 rounded-xl p-3 text-center ">
+        <div class="bg-gray-200 border-4 border-gray-800 rounded-xl p-3 text-center ">
             <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-gray-700"><span
                             class="border-b-4 border-green-800"><a href="#">Reserveringen</a></span></h2>
                 <div class="mb-4"><br>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="../Reserveer/createReservering.php">Create</a></span>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="../Reserveer/readReservering.php">Read</a></span>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="../Reserveer/readReservering.php">Update/Delete</a></span>
                 </div>
             </div>
         </div>
-        <div class="border-4 border-gray-800 rounded-xl p-3 text-center ">
+        <div class="bg-gray-200 border-4 border-gray-800 rounded-xl p-3 text-center ">
             <div class="rounded-xl"><h2 class="text-3xl font-bold hover:text-gray-700"><span
                             class="border-b-4 border-green-800"><a href="#">Ezels</a></span></h2>
                 <div class="mb-4"><br>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="">Create</a></span>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="">Read</a></span>
-                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800"><a
+                    <span class="hover:bg-gray-800 hover:text-white p-3 border-2 rounded-lg text-center border-green-800 bg-white"><a
                                 href="">Update/Delete</a></span>
                 </div>
             </div>
