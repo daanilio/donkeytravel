b024615c7503d3d5fdde7e8faaf05dd5db7ed39b
FlyingWumpus <94541665+FlyingWumpus@users.noreply.github.com>
2023-10-26 14:16:36 +0200

    Add composer autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fc649c6eb4dd1bf6acdcdeed991a723e1248a2e3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fc649c6eb4dd1bf6acdcdeed991a723e1248a2e3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
