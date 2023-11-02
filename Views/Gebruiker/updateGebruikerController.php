93c256f8df7636b19d2f0d459bd5a762102b02bb
FlyingWumpus <94541665+FlyingWumpus@users.noreply.github.com>
2023-11-02 13:25:16 +0100

    Add update gebruiker function for staff

--- Views/Gebruiker/updateGebruikerController.php
+++ Views/Gebruiker/updateGebruikerController.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gebruiker verwijderd</title>
+    <title>Update</title>
 </head>
 <?php
 
@@ -14,19 +14,18 @@ require_once '../../Models/Gebruikers.php';
 use Models\Gebruikers;
 
 $id = $_POST["id"];
+$voornaam = $_POST["voornaam"];
+$achternaam = $_POST["achternaam"];
+$email = $_POST["email"];
+$functie = $_POST["functie"];
 
-$gebruiker = new Gebruikers();
+$gebruiker = new Gebruikers($voornaam, $achternaam, $email, $functie);
 ?>
 
 <body class="flex min-h-screen justify-between flex-col">
 <?php include '../../Components/header.php'; ?>
 <main class="py-18 px-64 flex justify-center">
-    <div class="">
-        <h2 class="text-center mb-8">Deze gebruiker is verwijderd:<br></h2>
-            <?php $gebruiker->delete($id); ?>
-        <br><br>
-        <p class="text-center"><a  href='../index.php'>Ga terug naar de hoofdpagina</a></p>
-    </div>
+    <?php $gebruiker->update($id); ?>
 </main>
 <?php include '../../Components/footer.php'; ?>
 
