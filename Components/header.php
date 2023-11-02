c4de9f5306d086344339f39b0139b0855ab8c3b6
Lisaa-vd <vandongenlisa@hotmail.com>
2023-11-02 15:52:13 +0100

    Links aangepast

--- Components/header.php
+++ Components/header.php
@@ -8,14 +8,14 @@
     <nav class="sticky top-0 w-full py-4 px-2 border-x-4 border-green-800 border-b-4 bg-gray-800 grid grid-cols-4">
         <div class="flex">
             <p class="font-bold text-white uppercase text-xl bg-green-800 px-3 py-1">
-                <a href="../../Views/home.php">Donkey travel</a>
+                <a href="../../Views/index.php">Donkey travel</a>
             </p>
         </div>
         <div class="flex justify-center sm:flex sm:items-center sm:w-auto col-span-2">
             <div class="sm:flex-row text-sm uppercase">
-                <a href="../home.php#informatie"
+                <a href="../index.php#informatie"
                    class="hover:bg-gray-600 hover:text-white rounded py-2 px-4 mx-2">Informatie</a>
-                <a href="../home.php#overOns" class="hover:bg-gray-600 hover:text-white rounded py-2 px-4 mx-2">Over
+                <a href="../index.php#overOns" class="hover:bg-gray-600 hover:text-white rounded py-2 px-4 mx-2">Over
                     ons</a>
                 <a href="./createReservering.php" class="hover:bg-gray-600 hover:text-white rounded py-2 px-4 mx-2">Reserveer</a>
             </div>
