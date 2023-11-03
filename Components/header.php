30c1356f3a3eb8f89dd42f0af1d9a004b8c2817c
Lisaa-vd <vandongenlisa@hotmail.com>
2023-11-03 19:52:41 +0100

    header fix

--- Components/header.php
+++ Components/header.php
@@ -28,8 +28,12 @@
                 echo '<a href="../../Views/Login/login.php" class="hover:bg-gray-600 hover:text-white rounded py-2 px-4 mx-2">Inloggen</a>';
             }
             ?>
-
-            <img class="flex m-0 p-0" src="../../Images/person.png" alt="account icon" style="max-width: 40px;">
+            <?php
+            if (isset($_SESSION['id']) && $_SESSION['email'] && ($_SESSION['functie'] != 'medewerker')) { ?>
+                <a href="../Gasten/MDT.php"><img class="flex m-0 p-0" src="../../Images/person.png" alt="account icon" style="max-width: 40px;"></a>
+            <?php }else { ?>
+                <a href="../Medewerker/panel.php"><img class="flex m-0 p-0" src="../../Images/person.png" alt="account icon" style="max-width: 40px;"></a>
+            <?php } ?>
         </div>
     </nav>
 </header>
